cbb64d22f8b17946a660b1e0e54cbcb47f686bed
willowcheng <e.wingus@Gmail.com>
2015-08-01 18:41:47 -0400

    Clean up code and custom background view

--- Stormy/WeeklyTableViewController.swift
+++ Stormy/WeeklyTableViewController.swift
@@ -10,8 +10,22 @@ import UIKit
 
 class WeeklyTableViewController: UITableViewController {
 
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    @IBOutlet weak var currentWeatherIcon: UIImageView!
+    @IBOutlet weak var currentPrecipitationLabel: UILabel!
+    @IBOutlet weak var currentTemperatureRangeLabel: UILabel!
+    
+    // Location coordinates
+    let coordinate: (lat: Double, lon: Double) = (45.416587,-75.66772)
+    
+    // TODO: Enter your API key here
+    private let forecastAPIKey = "fb9bc4b82191b64d13b661b6897f06ea"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.backgroundView = BackgroundView()
+        retrieveWeatherForecast()
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,6 +45,35 @@ class WeeklyTableViewController: UITableViewController {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
         return 0
+    }
+    
+    // MARK: - Weather Fetching
+    
+    func retrieveWeatherForecast() {
+        let forecastService = ForecastService(APIKey: forecastAPIKey)
+        forecastService.getForecast(coordinate.lat, lon: coordinate.lon) {
+            (let currently) in
+            
+            if let currentWeather = currently {
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    
+                    if let temperature = currentWeather.temperature {
+                        self.currentTemperatureLabel?.text = "\(temperature)º"
+                    }
+                    
+                    if let precipitation = currentWeather.precipProbability {
+                        self.currentPrecipitationLabel?.text = "\(precipitation)%"
+                    }
+                    
+                    if let icon = currentWeather.icon {
+                        self.currentWeatherIcon?.image = icon
+                    }
+                    
+                }
+                
+            }
+        }
     }
 
 }
