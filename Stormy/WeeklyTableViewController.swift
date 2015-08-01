abf8acecfc18d207a828350f55af359e87de551a
willowcheng <e.wingus@Gmail.com>
2015-08-01 17:40:54 -0400

    Add table view controller

---
+++ Stormy/WeeklyTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  WeeklyTableViewController.swift
+//  Stormy
+//
+//  Created by Liu Cheng on 2015-08-01.
+//  Copyright (c) 2015 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class WeeklyTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return 0
+    }
+
+}
