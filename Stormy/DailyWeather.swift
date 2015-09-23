ab45a68f5936557c92330bf4f2b8a6c1e5d59bf4
willowcheng <e.wingus@Gmail.com>
2015-09-23 19:25:04 -0400

    Working with dates and times

--- Stormy/DailyWeather.swift
+++ Stormy/DailyWeather.swift
@@ -18,13 +18,15 @@ struct DailyWeather {
     let summary: String?
     var icon: UIImage? = UIImage(named: "default.png")
     var largeIcon: UIImage? = UIImage(named: "default_large.png")
-    let sunriseTime: String?
-    let sunsetTime: String?
-    let day: String?
+    var sunriseTime: String?
+    var sunsetTime: String?
+    var day: String?
+    let dateFormatter = NSDateFormatter()
     
     init(dailyWeatherDict: [String: AnyObject]) {
         
         maxTemperature = dailyWeatherDict["temperatureMax"] as? Int
+        minTemperature = dailyWeatherDict["temperatureMin"] as? Int
         if let humidityFloat = dailyWeatherDict["humidity"] as? Double {
             humidity = Int(humidityFloat * 100)
         } else {
@@ -40,6 +42,35 @@ struct DailyWeather {
         let iconEnum = Icon(rawValue: iconString) {
             (icon, largeIcon) = iconEnum.toImage()
         }
+        if let sunriseDate = dailyWeatherDict["sunriseTime"] as? Double {
+            sunriseTime = timeStringFromUnixTime(sunriseDate)
+        } else {
+            sunriseTime = nil
+        }
+        if let sunsetDate = dailyWeatherDict["sunsetTime"] as? Double {
+            sunsetTime = timeStringFromUnixTime(sunsetDate)
+        } else {
+            sunsetTime = nil
+        }
+        if let time = dailyWeatherDict["time"] as? Double {
+            day = dayStringFromTime(time)
+        } else {
+            day = nil
+        }
     }
     
+    func timeStringFromUnixTime(unixTime: Double) -> String {
+        let date = NSDate(timeIntervalSince1970: unixTime)
+        
+        // Returns date formatted as 12 hour time.
+        dateFormatter.dateFormat = "hh:mm a"
+        return dateFormatter.stringFromDate(date)
+    }
+    
+    func dayStringFromTime(time: Double) -> String {
+        let date = NSDate(timeIntervalSince1970: time)
+        dateFormatter.locale = NSLocale(localeIdentifier: NSLocale.currentLocale().localeIdentifier)
+        dateFormatter.dateFormat = "EEEE"
+        return dateFormatter.stringFromDate(date)
+    }
 }
